3ded72053bd38faff99740dd63df915d5f87cbbf
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2024-08-09 17:28:24 -0400

    Fix a few bugs in auth routine

--- app/main.py
+++ app/main.py
@@ -12,14 +12,14 @@ models.Base.metadata.create_all(bind=database.engine)
 
 @app.post("/token", response_model=schemas.Token)
 async def login_for_access_token(db: Session = Depends(database.get_db), form_data: OAuth2PasswordRequestForm = Depends()):
-    user = auth.authenticate_user(db, form_data.email, form_data.password)
+    user = auth.authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=400,
             detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
     access_token = auth.create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
