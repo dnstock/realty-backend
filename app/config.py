1d130d6006ed28d622f493606ecdaff46bb60278
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2024-08-25 18:19:22 -0400

    Improve and refactor config to comply with Pydantic v2, rename `config` dir to `envs` dir

--- app/config.py
+++ app/config.py
@@ -1,52 +1,46 @@
-from pydantic import ValidationError
+from pydantic import Field, model_validator
 from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
 import os
 
 # Absolute path of `configs` directory relative to this file
-configs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config'))
-
-# Load the global .env file
-load_dotenv(os.path.join(configs_dir, ".env"))
+env_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'envs'))
 
 class Settings(BaseSettings):
-    environment: str
-    database_user: str
-    database_password: str
-    database_name: str
-    database_host: str = 'localhost'
-    database_port: int = 5432
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
-    database_url: str = ''
-    is_docker: bool = False  # Will be set to True if running in Docker
+    environment: str = Field(..., description="The environment type (development, production, etc.)")
+    database_user: str = Field(..., description="Database user")
+    database_password: str = Field(..., description="Database password")
+    database_name: str = Field(..., description="Database name")
+    database_host: str = Field('localhost', description="Database host")
+    database_port: int = Field(5432, description="Database port")
+    secret_key: str = Field(..., description="Secret key for JWT")
+    algorithm: str = Field(..., description="Algorithm for JWT")
+    access_token_expire_minutes: int = Field(..., description="Access token expiry time in minutes")
+    database_url: str = Field('', description="Database URL", exclude=True)
+    is_docker: bool = Field(False, description="Set to True if running in Docker")
     
-    @classmethod
-    def validate_env_vars(cls):
-        required_vars = [
-            'DATABASE_USER', 'DATABASE_PASSWORD', 'DATABASE_NAME', 
-            'SECRET_KEY', 'ALGORITHM', 'ACCESS_TOKEN_EXPIRE_MINUTES'
-        ]
-        missing_vars = [var for var in required_vars if not os.getenv(var)]
-        if missing_vars:
-            raise ValidationError(f"Missing required environment variables: {', '.join(missing_vars)}")
-
-    def __init__(self, **kwargs):
-        # Load the environment-specific .env file
-        env_file = f".env.{os.getenv('ENVIRONMENT')}"
-        load_dotenv(os.path.join(configs_dir, env_file), override=True)
+    model_config = {
+        'env_file': [
+            os.path.join(env_file_path, ".env"),
+            os.path.join(env_file_path, f".env.{os.getenv('ENVIRONMENT')}")
+        ],
+        'env_file_encoding': 'utf-8',
+        'case_sensitive': False,
+        # 'validate_default': True,
+    }
+    
+    @model_validator(mode='after')
+    def adjust_database_host_and_url(cls, values):
+        # Adjust database_host if running in Docker
+        if values.database_host == 'localhost' and values.is_docker:
+            values.database_host = 'host.docker.internal'
         
-        # Validate environment
-        self.validate_env_vars()
-        super().__init__(**kwargs)
+        # Assemble the database URL
+        values.database_url = (
+            f"postgresql://{values.database_user}:{values.database_password}"
+            f"@{values.database_host}:{values.database_port}/{values.database_name}"
+        )
         
-        # Set default database host for Docker
-        if self.database_host == 'localhost' and self.is_docker:
-            self.database_host = 'host.docker.internal'
-        
-        # Create the database URL
-        self.database_url = f"postgresql://{self.database_user}:{self.database_password}@{self.database_host}:{self.database_port}/{self.database_name}"
+        return values
 
 # Initialize settings
 settings = Settings()
