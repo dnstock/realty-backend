c2122c2ae433d8a9e9b38902a0b603e8ba988bc3
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2024-09-08 19:14:33 -0400

    Update Alembic import paths

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,8 @@
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 from alembic import context
-from app.core.config import settings
-from app.db import Base
+from core import settings
+from db import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
