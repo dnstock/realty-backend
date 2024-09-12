bd430a3a41bf7bd9ce56ff7030e1eeeb1c55ccce
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2024-09-12 15:27:40 -0400

    Add explicit type setting for modules

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,3 +1,4 @@
+from types import ModuleType
 from fastapi import FastAPI, APIRouter
 from .errors import *
 from .middleware import *
@@ -5,10 +6,11 @@ from .endpoints import auth, users, properties, buildings, units, leases, tenant
 
 router = APIRouter()
 
-# Register routes for API v1
-for module in [auth, users, properties, buildings, units, leases, tenants, insurances]:
-    module_name = module.__name__.split('.')[-1]  # Get the module name without the package name
-    router.include_router(getattr(module, 'router'), prefix=f"/{module_name}", tags=[module_name])
+modules: list[ModuleType] = [auth, users, properties, buildings, units, leases, tenants, insurances]
+
+for module in modules:
+    module_name: str = module.__name__.split('.')[-1]  # Get the module name without the package name
+    router.include_router(getattr(module, 'router', APIRouter()), prefix=f'/{module_name}', tags=[module_name])
     
 # Register middleware for API v1
 def register_middleware(app: FastAPI) -> None:
@@ -20,7 +22,7 @@ def register_error_handlers(app: FastAPI) -> None:
     v1_unhandled_exception_handler(app)
 
 __all__ = [
-    "router",
-    "register_middleware",
-    "register_error_handlers"
+    'router',
+    'register_middleware',
+    'register_error_handlers'
 ]
