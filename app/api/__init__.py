06e1c0929bff99e2368e81ff87458e78aba2671f
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2024-09-12 15:25:45 -0400

    Comment out v1-specific API configs for now

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,25 +1,22 @@
 from fastapi import FastAPI, APIRouter
 from .errors import *
 from .middleware import *
-from .v1 import (
-    router as v1_router, 
-    register_middleware as register_v1_middleware,
-    register_error_handlers as register_v1_error_handlers
-)
+from .v1 import router as v1_router
 
 api_router = APIRouter()
 
 # Include the v1 API routes
-api_router.include_router(v1_router, prefix="/v1")
+api_router.include_router(v1_router, prefix='/v1')
 
 # Register middleware for the API
 def register_middleware(app: FastAPI) -> None:
-    cors_middleware(app)
+    cors_middleware(app)  # Must be first
     db_session_middleware(app)
     request_id_middleware(app)
     
-    # Register v1-specific middleware
-    register_v1_middleware(app)
+    # Uncomment the lines below to include v1-specific middleware
+    #from .v1 import register_middleware as register_v1_middleware
+    #register_v1_middleware(app)
 
 # Register error handlers for the API
 def register_error_handlers(app: FastAPI) -> None:
@@ -28,11 +25,12 @@ def register_error_handlers(app: FastAPI) -> None:
     common_sqlalchemy_exception_handler(app)
     common_unhandled_exception_handler(app)
     
-    # Register v1-specific error handlers
-    register_v1_error_handlers(app)
+    # Uncomment the lines below to include v1-specific error handlers
+    #from .v1 import register_error_handlers as register_v1_error_handlers
+    #register_v1_error_handlers(app)  
 
 __all__ = [
-    "api_router",
-    "register_middleware",
-    "register_error_handlers"
+    'api_router',
+    'register_middleware',
+    'register_error_handlers'
 ]
