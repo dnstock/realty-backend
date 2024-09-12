3de15c9569d2779eadf45e33208c28c9bb8d0fd9
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2024-09-12 15:26:45 -0400

    Properly handle different data types in exception body

--- app/api/errors.py
+++ app/api/errors.py
@@ -1,3 +1,4 @@
+from starlette.datastructures import FormData
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
@@ -9,16 +10,25 @@ def common_http_exception_handler(app: FastAPI) -> None:
         log_middleware_exception(exc, request)
         return JSONResponse(
             status_code=exc.status_code,
-            content={"detail": exc.detail},
+            content={'detail': exc.detail},
         )
     app.add_exception_handler(HTTPException, handler) # type: ignore (only HTTPException is allowed)
 
 def common_validation_exception_handler(app: FastAPI) -> None:
     def handler(request: Request, exc: RequestValidationError) -> JSONResponse:
         log_middleware_exception(exc, request)
+        body = exc.body
+        if isinstance(body, FormData):
+            body = dict(body)  # Convert FormData to a dictionary (serializable)
+        elif isinstance(body, (dict, list)):
+            pass  # Already serializable
+        elif isinstance(body, (str, bytes)):
+            body = {'raw_body': body}  # Wrap raw data in a dictionary to make it serializable
+        else:
+            body = None  # Fallback in case body type is unexpected or unhandled
         return JSONResponse(
             status_code=422,
-            content={"detail": exc.errors(), "body": exc.body},
+            content={'detail': exc.errors(), 'body': body},
         )
     app.add_exception_handler(RequestValidationError, handler) # type: ignore (only RequestValidationError is allowed)
 
@@ -27,7 +37,7 @@ def common_sqlalchemy_exception_handler(app: FastAPI) -> None:
         log_middleware_exception(exc, request)
         return JSONResponse(
             status_code=400,
-            content={"detail": "Database integrity error occurred."},
+            content={'detail': 'Database integrity error occurred.'},
         )
     app.add_exception_handler(IntegrityError, handler) # type: ignore (only IntegrityError is allowed)
 
@@ -36,6 +46,6 @@ def common_unhandled_exception_handler(app: FastAPI) -> None:
         log_middleware_exception(exc, request)
         return JSONResponse(
             status_code=500,
-            content={"detail": "An unexpected server error occurred."},
+            content={'detail': 'An unexpected server error occurred.'},
         )
     app.add_exception_handler(Exception, handler)
