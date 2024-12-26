f91e900928e5322cd9b5e4d77dbd47ae5b7d56e8
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2024-12-25 23:12:41 -0500

    Update create table script to take a database name parameter

--- scripts/create_tables.py
+++ scripts/create_tables.py
@@ -1,19 +1,25 @@
+# USAGE NOTE:
+#   Run this script with the command `./scripts/create_tables` from the root directory
+
 '''
-Creates database tables as defined in the `models.py` file.
 This does not handle database migrations, and therefore
-should likely never be used. Instead use Alembic.
-
-Usage:
-    python create_tables.py
+going through Alembic is recommended for production use.
 '''
 
+import sys
 from sqlalchemy import create_engine
-from app.models import Base
-from app.config import settings
+from db import Base
 
-# Create the engine using the database URL
-engine = create_engine(settings.database_url)
+if len(sys.argv) != 2:
+    print("Error: Missing database name")
+    sys.exit(1)
 
-# Create all tables in the database
-#(UNCOMMENT THE LINE BELOW BEFORE RUNNING THIS SCRIPT)
-#Base.metadata.create_all(bind=engine)
+database = sys.argv[1]
+engine = create_engine(f'postgresql+psycopg://admin:admin@localhost/{database}')
+
+print(f'Creating tables in database: {database}')
+
+# Base.metadata.drop_all(bind=engine) # Uncomment to drop all tables first
+Base.metadata.create_all(bind=engine)
+
+print('Tables created successfully!')
